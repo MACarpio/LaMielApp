1227103be17f1fa09ce8d09127c44019064bd69d
Mauricio Apaza <mpacarpio@gmail.com>
2021-11-06 02:19:09 -0500

    pago controller

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -16,11 +16,10 @@ namespace LaMielApp.Data
         }
         public DbSet<LaMielApp.Models.Contactanos> DataContactanos { get; set; }
 
-        public DbSet<LaMielApp.Models.Product> DataProduct {get; set;}
-        public DbSet<LaMielApp.Models.Pago> DataPago {get; set;}
-        public DbSet<LaMielApp.Models.Pedido> DataPedido {get; set;}
-
-
-        public DbSet<LaMielApp.Models.Proforma> DataProforma {get; set;}
+        public DbSet<LaMielApp.Models.Product> DataProduct { get; set; }
+        public DbSet<LaMielApp.Models.Pago> DataPago { get; set; }
+        public DbSet<LaMielApp.Models.Pedido> DataPedido { get; set; }
+        public DbSet<LaMielApp.Models.DetallePedido> DataDetallePedido { get; set; }
+        public DbSet<LaMielApp.Models.Proforma> DataProforma { get; set; }
     }
 }
--- Models/DetallePedido.cs
+++ Models/DetallePedido.cs
@@ -6,16 +6,15 @@ using System.Threading.Tasks;
 
 namespace LaMielApp.Models
 {
-    [Table("t_proforma")]
-    public class Proforma
-    {       
+    [Table("t_order_detail")]
+    public class DetallePedido
+    {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Column("id")]
-        public int Id { get; set; }
-        public String UserID {get; set;}
+        public int ID {get; set;}
         public Product Producto {get; set;}
         public int Quantity{get; set;}
         public Decimal Price { get; set; }
-        public String Status { get; set; } = "PENDIENTE";
+        public Pedido pedido {get; set;}
     }
 }
